aed9dce0090c275e68469aa68dbb00861c124f50
oryo23-meet <109899438+oryo23-meet@users.noreply.github.com>
2022-07-24 15:17:41 +0300

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+print("hello y2")
+name=input("whats ur name?")
+print('hello ' + name)
+age=input("how old are you?")
+if age == 16:
+	print("cool")
+elif age > 16:
+	print('such a fetus')
+elif age> 16:
+	print('daddy')
